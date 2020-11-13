20c43cbacdaca1d88f4fe49750225731fa3c47fe
Sebastian Knackstedt <sebknack@gmail.com>
2020-11-13 19:33:07 +0100

    Better first-person transformation for usable items.

--- src/main/java/com/github/se7_kn8/gates/data_gen/ItemModels.java
+++ src/main/java/com/github/se7_kn8/gates/data_gen/ItemModels.java
@@ -4,9 +4,12 @@ package com.github.se7_kn8.gates.data_gen;
 import com.github.se7_kn8.gates.Gates;
 import com.github.se7_kn8.gates.GatesItems;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.util.IItemProvider;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.client.model.generators.ModelBuilder;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.fml.RegistryObject;
 
 
 public class ItemModels extends ItemModelProvider {
@@ -17,15 +20,15 @@ public class ItemModels extends ItemModelProvider {
 
 	@Override
 	protected void registerModels() {
-		addSimpleItemModel(GatesItems.FREQUENCY_CHANGER.get());
-		addSimpleItemModel(GatesItems.PORTABLE_REDSTONE_TRANSMITTER.get());
-		addSimpleItemModel(GatesItems.REDSTONE_TORCH_PEARL.get());
+		addSimpleItemModel(GatesItems.FREQUENCY_CHANGER).transforms().transform(ModelBuilder.Perspective.FIRSTPERSON_RIGHT).rotation(-45,0,0).scale(0.8f).translation(6,0,-7);
+		addSimpleItemModel(GatesItems.PORTABLE_REDSTONE_TRANSMITTER).transforms().transform(ModelBuilder.Perspective.FIRSTPERSON_RIGHT).rotation(-45,0,0).scale(0.8f).translation(6,0,-7);;
+		addSimpleItemModel(GatesItems.REDSTONE_TORCH_PEARL);
 
 	}
 
-	private void addSimpleItemModel(IItemProvider item) {
-		String name = item.asItem().getRegistryName().getPath();
-		withExistingParent(name, mcLoc("item/generated")).texture("layer0", modLoc("item/" + name));
+	private ItemModelBuilder addSimpleItemModel(RegistryObject<? extends Item> item) {
+		String name = item.getId().getPath();
+		return withExistingParent(name, mcLoc("item/generated")).texture("layer0", modLoc("item/" + name));
 	}
 
 }
